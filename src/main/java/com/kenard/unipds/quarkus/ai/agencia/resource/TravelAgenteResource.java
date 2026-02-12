7c0dbc43bf0717f89ad9563747e91d670e19ba66
Kenard RS <kenardrs@gmail.com>
2026-02-12 16:12:00 -0300

    add pgvector to persist vectors

--- src/main/java/com/kenard/unipds/quarkus/ai/agencia/resource/TravelAgenteResource.java
+++ src/main/java/com/kenard/unipds/quarkus/ai/agencia/resource/TravelAgenteResource.java
@@ -1,5 +1,6 @@
-package com.kenard.unipds.quarkus.ai.agencia.service.resource;
+package com.kenard.unipds.quarkus.ai.agencia.resource;
 
+import com.kenard.unipds.quarkus.ai.agencia.service.PackageExpert;
 import com.kenard.unipds.quarkus.ai.agencia.service.TravelAgenteAssistant;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
@@ -14,10 +15,22 @@ public class TravelAgenteResource {
     @Inject
     TravelAgenteAssistant assistant;
 
+    @Inject
+    PackageExpert packageExpert;
+
     @POST
+    @Path("/assistant")
     @Consumes(MediaType.TEXT_PLAIN)
     @Produces(MediaType.TEXT_PLAIN)
     public String ask(String question) {
         return assistant.chat(question);
+    }
+
+    @POST
+    @Path("/expert")
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String askExpert(String question) {
+        return packageExpert.chat("session-123", question);
     }
 }
